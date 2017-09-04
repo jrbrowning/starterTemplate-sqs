bce40a8ec529c6040e41b92a7543f2ec01f27fe8
Jeff Browning <jeff@craftcoded.com>
2017-09-04 15:26:08 -0700

    webpack config files app.js, config.js.   Initial _varialbles partial and main.scss for webpack

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: ['./src/js/app.js', './src/scss/main.scss'],
+  output: {
+    filename: 'dist/bundle.js'
+  },
+  module: {
+    rules: [
+      /*
+      your other rules for JavaScript transpiling go in here
+      */
+      { // regular css files
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          use: ['style-loader','css-loader']
+        }),
+        //options: {
+        //  includePath: "./css/plain_css.css"
+        //}
+      },
+      { // sass / scss loader for webpack
+        test: /\.(sass|scss)$/,
+        loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin({ // define where to save the file
+      filename: 'public/css/[name].css',
+      allChunks: true,
+    }),
+  ],
+};
